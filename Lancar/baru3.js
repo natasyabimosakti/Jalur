f8ea52216215178d8d6c3dfd6612e62ad488a27a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-28 15:00:48 +0700

    Update baru3.js

--- Lancar/baru3.js
+++ Lancar/baru3.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Lancar Baru1
+// @name         Lancar Baru3
 // @namespace    http://tampermonkey.net/
 // @version      3.00
 // @description  try to take over the world!
