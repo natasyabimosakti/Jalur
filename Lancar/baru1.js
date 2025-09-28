1d92e05e84ebf11192dfba6d9d593f7ab64e58d1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-28 14:58:12 +0700

    Update baru1.js

--- Lancar/baru1.js
+++ Lancar/baru1.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Cuwil 1
+// @name         Lancar Baru1
 // @namespace    http://tampermonkey.net/
 // @version      3.00
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil1.js
-// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Novi91/main/Cuwil/Cuwil1.js
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Jalur/refs/heads/main/Lancar/baru1.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Jalur/refs/heads/main/Lancar/baru1.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
